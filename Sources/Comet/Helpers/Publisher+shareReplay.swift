9ca738b3007edae0d3300b501e5ce26dddcf3f5a
Tuan Tu Do <tuanthatsmyname@gmail.com>
2021-06-12 11:42:39 +0200

    Add shareReplay operator

--- Sources/Comet/Helpers/Publisher+shareReplay.swift
+++ Sources/Comet/Helpers/Publisher+shareReplay.swift
@@ -1,9 +1,16 @@
 //
-//  File.swift
+//  Publisher+shareReplay.swift
 //  Comet
 //
 //  Created by Tuan Tu Do on 12.06.2021.
 //  Copyright © 2021 Etnetera. All rights reserved.
 //
 
-import Foundation
+import Combine
+
+extension Publisher {
+    func share(replay count: Int) -> Publishers.Autoconnect<Publishers.Multicast<Self, ReplaySubject<Output, Failure>>> {
+        multicast { ReplaySubject(bufferSize: count) }
+            .autoconnect()
+    }
+}
