0059c7956e61afc077d683a728cfd6a3891f5b08
Milan Čáp <milan.cap@etnetera.cz>
2022-01-14 10:43:03 +0100

    Make members in TokenProvidingHttpError public

--- Sources/Comet/CometClient/CometClientHttpError.swift
+++ Sources/Comet/CometClient/CometClientHttpError.swift
@@ -5,6 +5,11 @@
 import Foundation
 
 public struct CometClientHttpError {
+    public init(code: Int, data: Data) {
+        self.code = code
+        self.data = data
+    }
+
     public let code: Int
     public let data: Data
 }
